604f60477e1580542c29ac18ce57762e27ab2456
Alexander Campbell <acampbell2231@gmail.com>
2017-09-09 15:04:31 -0600

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,12 @@
 Rails.application.routes.draw do
   root to: 'static_pages#show'
   
-  devise_for :users
 
-<<<<<<< HEAD
   resources :posts do
     resources :comments
   end
 
   resources :bios
-=======
-  resources :accounts do
-    resources :posts
-  end
-
-  resources :bios
-
-  scope 'posts/:post_id', as: 'post' do
-    resources :comments, only: [:new, :create]
-  end
->>>>>>> this is about to break all of my stuff
+  devise_for :users
 
 end
